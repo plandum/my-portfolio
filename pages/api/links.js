ea6a0c784c6f3c6d0bf085e56901b5059c597f12
plandum <temik.0465@mail.ru>
2022-11-12 17:38:16 +0300

    V1

---
+++ pages/api/links.js
@@ -0,0 +1,45 @@
+const links = [
+	{
+		_id: 'Link_1',
+		icon: {
+			path: '/icons/ВКонтакте.png',
+			width: 160
+		},
+		gradient: {
+			from: '#a044c1',
+			to: '#6f1fbf'
+		},
+		link: 'https://vk.com/plandum',
+		title: 'ВКонтакте'
+	},
+	{
+		_id: 'Link_1',
+		icon: {
+			path: '/icons/ВКонтакте.png',
+			width: 160
+		},
+		gradient: {
+			from: '#a044c1',
+			to: '#6f1fbf'
+		},
+		link: 'https://vk.com/plandum',
+		title: 'ВКонтакте'
+	},
+	{
+		_id: 'Link_1',
+		icon: {
+			path: '/icons/ВКонтакте.png',
+			width: 160
+		},
+		gradient: {
+			from: '#a044c1',
+			to: '#6f1fbf'
+		},
+		link: 'https://vk.com/plandum',
+		title: 'ВКонтакте'
+	}
+]
+
+export default function handler(req, res) {
+	res.status(200).json(links)
+}
