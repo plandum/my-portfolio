e017272a562b9c44a17f2c0e094948f7a18eff95
plandum <temik.0465@mail.ru>
2022-11-12 17:45:13 +0300

    v3

--- pages/api/links.js
+++ pages/api/links.js
@@ -37,6 +37,19 @@ const links = [
 		},
 		link: 'https://vk.com/plandum',
 		title: 'ВКонтакте'
+	},
+	{
+		_id: 'Link_1',
+		icon: {
+			path: '/icons/ВКонтакте.png',
+			width: 160
+		},
+		gradient: {
+			from: '#a044c1',
+			to: '#6f1fbf'
+		},
+		link: 'https://vk.com/plandum',
+		title: 'ВКонтакте'
 	}
 ]
 
